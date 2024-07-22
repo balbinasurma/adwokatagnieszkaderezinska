80f6394ff70754a4c2d1477121a9de3fb8aaa018
balbinasurma <59312307+balbinasurma@users.noreply.github.com>
2024-07-22 16:45:02 +0200

    Add files via upload

---
+++ klinikachirurgireki.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('mobile-menu');
+    const nav = document.querySelector('.nav');
+
+    menuToggle.addEventListener('click', function() {
+        nav.classList.toggle('active');
+    });
+});
